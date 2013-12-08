4b16f6b3e7cee0d297512a246053563f8df4d54b
Matthias Dietrich <perl@rb.ly>
2013-12-08 21:40:19 +0100

    Fixed some stuff for installing Perl

--- perl/install.sh
+++ perl/install.sh
@@ -4,6 +4,7 @@ if test ! $(which perlbrew)
 then
   echo "  Installing perlbrew and perl-blead for you."
   curl -L http://install.perlbrew.pl -s | bash > /tmp/perlbrew-install.log
+  source ~/perl5/perlbrew/etc/bashrc
   perlbrew install perl-blead
   perlbrew use perl-blead
 fi
