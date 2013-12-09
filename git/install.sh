ae2c17c138f6fc0998877b4b854ae92762c03e4c
Matthias Dietrich <perl@rb.ly>
2013-12-09 12:00:15 +0100

    Conditionally install gist (for configuration)

--- git/install.sh
+++ git/install.sh
@@ -1,7 +1,11 @@
 #!/bin/sh
 
 # Install hub and gist
-brew install hub gist
+brew install hub
 
-# Now grab an OAuth2 token for gist
-gist --login
+if test ! $(which gist)
+then
+    # Install gist and grab an OAuth2 token
+    brew install gist
+    gist --login
+fi
