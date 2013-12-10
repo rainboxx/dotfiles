ace85d12e7cee2b0157286b92b13a0317399b6fc
Matthias Dietrich <perl@rb.ly>
2013-12-10 13:29:26 +0100

    Change path for perl lib from relative to absolute
    Strange thing, didn't work as a relative path — if someone finds out why, I'd like to know it :-)

--- perl/perlbrew.zsh
+++ perl/perlbrew.zsh
@@ -5,6 +5,6 @@ then
   export PERL_LOCAL_LIB_ROOT="~/perl5:$PERL_LOCAL_LIB_ROOT";
   export PERL_MB_OPT="--install_base "~/perl5"";
   export PERL_MM_OPT="INSTALL_BASE=~/perl5";
-  export PERL5LIB="~/perl5/lib/perl5:$PERL5LIB";
+  export PERL5LIB="/Users/md/perl5/lib/perl5:$PERL5LIB";
   export PATH="~/perl5/bin:$PATH";
 fi
